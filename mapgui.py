2420b67d128bd4c3b3f7965e6b749b0cb9664b85
Student <student@C17775943.offcampusnetwork.co.uk>
2019-02-11 16:24:40 +0000

    Now draws lines between points

--- mapgui.py
+++ mapgui.py
@@ -9,38 +9,28 @@ class MapGUI:
         master.title("Map GUI")
         master.geometry("150x150")
 
-        #self.label = Label(master, text="Map GUI!")
-        #self.label.pack()
-
-        self.greet_button = Button(master, text="Import Map", command = self.showMap)
-        self.greet_button.pack()
+        self.import_map_button = Button(master, text="Import Map", command = self.showMap)
+        self.import_map_button.pack()
 
         self.close_button = Button(master, text="Close", command = master.quit)
         self.close_button.pack()
-
-        #self.mapCanvas = Canvas(master, width = 800, height = 1000)
-        #self.mapCanvas.pack()
 
 
     def drawMap(self):
         toDraw = self.getMap()
         centerCanvas = [400, 500]        
 
-
         for x in toDraw:
           self.mapCanvas.create_line(centerCanvas[0] + x[0], centerCanvas[1] + x[1], 100, 100)
-
-        #self.w.create_line(0, 0, 200, 100)
-        #self.w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-        #self.w.create_rectangle(50, 25, 150, 75, fill="blue")
         
 
     # Get map angles and distances from serial
     def getMap(self):
-        measurements = [ [0, 100],
+        measurements = [[0, 100],
                       [90, 100],
                       [180, 100],
-                      [270, 100]]
+                      [270, 100],
+                      [360, 100]]
         return measurements
 
     # Draw map on plot
@@ -48,14 +38,18 @@ class MapGUI:
 
         measurements = self.getMap()
         coords = []
+        x = []
+        y = []
 
         for measurement in measurements:
-          x, y = self.getCoords(measurement)
-          coords.append([x, y])
+         # x, y = self.getCoords(measurement)
+         # coords.append([x, y])
+          x.append(self.getXCoord(measurement))
+          y.append(self.getYCoord(measurement))
 
-        for coord in coords:
-          plt.plot(coord[0], coord[1], 'ro-')
-
+        #for coord in coords:
+        #  plt.plot([coord[0], 100], [coord[1], 150], 'ro-')
+        plt.plot(x, y)
         plt.show()
 
 
@@ -65,6 +59,13 @@ class MapGUI:
         y = measurement[1] * math.sin(math.radians(measurement[0]))
         return x, y
 
+    def getXCoord(self, measurement):
+        x = measurement[1] * math.cos(math.radians(measurement[0]))
+        return x
+
+    def getYCoord(self, measurement):
+        y = measurement[1] * math.sin(math.radians(measurement[0]))
+        return y
 
 
 root = Tk()
